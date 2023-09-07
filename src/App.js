190c0db0dc898eeed0894866981e1d8df8684c14
Nikesh Gupta <nikeshgupta130@gmail.com>
2023-09-07 13:18:41 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import CurrentLocation from "./currentLocation";
+import "./App.css";
+
+const App = () => {
+  return (
+    <>
+      <div className="container">
+        <CurrentLocation />
+      </div>
+      
+    </>
+  );
+}
+
+export default App;
